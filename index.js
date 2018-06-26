f0b5c986c2910eb72c110a5400a8b329f63a34f9
Issouf zabre <sheridanzabre@gmail.com>
2018-06-26 15:19:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var greeting = "Hello, everybody!";
 
+\\var test = "math, physics, scients"\\
+
+\\ var game = "the great game of baskeball"
+the great game of baskeball"is the best game ever"
+return game + "is the best game ever"
+
+\\return `${game} is the best game ever`
+
 var specialGuest = "Neil deGrasse Tyson"
   
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
@@ -7,3 +15,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}.`;
+
