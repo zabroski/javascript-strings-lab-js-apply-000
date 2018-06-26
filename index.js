da08f083040cfcf96cd60fc92b12ad8003ed05fb
Issouf zabre <sheridanzabre@gmail.com>
2018-06-26 15:20:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 var greeting = "Hello, everybody!";
 
-\\var test = "math, physics, scients"\\
-
-\\ var game = "the great game of baskeball"
-the great game of baskeball"is the best game ever"
-return game + "is the best game ever"
-
-\\return `${game} is the best game ever`
-
 var specialGuest = "Neil deGrasse Tyson"
   
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
